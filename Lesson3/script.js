aceb693c865014bf9096bc93f531feb1003cdee5
Valxshan <valenciangzishan@gmail.com>
2025-07-15 20:48:46 +0800

    Exercise 5

---
+++ Lesson3/script.js
@@ -0,0 +1,7 @@
+const companyName = "Metaa";
+
+let slicedCompanyName = companyName.substring(0, 4);
+console.log(slicedCompanyName);
+
+let upperCaseCompanyName = slicedCompanyName.toUpperCase();
+console.log(upperCaseCompanyName);
